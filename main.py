d79ff2d80a943a6f7d93402c17727acbf19381f4
Yassine-Moussa <yassine.moussa@edu.esiee.fr>
2025-09-29 19:02:05 +0000

    message

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#### Imports et définition des variables globales
-
+'''Imports et définition des variables globales'''
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
@@ -8,44 +7,55 @@ sys.setrecursionlimit(2000)
 #### Fonctions secondaires
 
 
-def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
+def artcode_i(s,):
+    """
+    liste de tuple
     Args:
         s (str): la chaîne de caractères à encoder
 
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
-
-    return [ ]
+    #votre code ici
+    n=len(s)
+    c=[s[0]]
+    occ=[1]
+    k=1
+    while  n>k:
+        if s[k]==s[k-1]:
+            occ[-1] += 1          
+        else:
+            occ.append(1)
+            c.append(s[k])
+        l=list(zip(c,occ))
+        k+=1
+    return l
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
+    """
     Args:
         s (str): la chaîne de caractères à encoder
 
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
-    """
-    
-    # votre code ici
-
+        """
+    #haut du code
+    n=len(s)
     # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
-
-#### Fonction principale
-
-
+    occ=[1]
+    k=1
+    if s=="":
+        return []
+    if k<n :
+        occ[-1] += 1
+    occ.insert(0, 1)  
+    return [(s[0],1)]+artcode_r(s[k:],)
+    #recherche nombre de caractères identiques au premier
+    #appel récursif
+""" Fonction principale"""
 def main():
+    "afficher le résultat souhaité avec les deux fonctions"
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
