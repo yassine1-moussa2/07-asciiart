ee8b32c1f66e87cec07e87d413f1c2f63d3fafc8
Yassine-Moussa <yassine.moussa@edu.esiee.fr>
2025-09-29 14:09:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#### Imports et définition des variables globales
+
+# Mandatory for the recursive solution to work on large inputs
+import sys
+sys.setrecursionlimit(2000)
+
+
+#### Fonctions secondaires
+
+
+def artcode_i(s):
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+
+    Args:
+        s (str): la chaîne de caractères à encoder
+
+    Returns:
+        list: la liste des tuples (caractère, nombre d'occurences)
+    """
+    
+    # votre code ici
+
+    return [ ]
+
+
+def artcode_r(s):
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+
+    Args:
+        s (str): la chaîne de caractères à encoder
+
+    Returns:
+        list: la liste des tuples (caractère, nombre d'occurences)
+    """
+    
+    # votre code ici
+
+    # cas de base
+    # recherche nombre de caractères identiques au premier
+    # appel récursif
+
+    return []
+    
+
+#### Fonction principale
+
+
+def main():
+    print(artcode_i('MMMMaaacXolloMM'))
+    print(artcode_r('MMMMaaacXolloMM'))
+
+if __name__ == "__main__":
+    main()
